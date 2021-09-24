854e1614a34c66d8ed19f8d03f0eb2695c88df45
Chrissie Sparling <gigglewrks@aol.com>
2021-09-24 16:18:38 -0700

    additional js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = getUserInput();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
@@ -14,3 +14,30 @@ var numericCharacters =  ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 var specialCharacters = [  '@',  '%',  '+',  '\\',  '/',  "'",  '!',  '#',  '$',  '^',  '?',  ':',  ',',  ')',  '(',  '}',  '{',  ']',  '[',  '~',  '-',  '_',  '.'];
 var lowerCase = [  'a',  'b',  'c',  'd',  'e',  "f",  'g',  'h',  'i',  'j',  'k',  'l',  'm',  'n',  'o',  'p',  'q',  'r',  's',  't',  'u',  'v',  'w',  'x',  'y',  'z'];
 var upperCase = [  'A',  'B',  'C',  'D',  'E',  "F",  'G',  'H',  'I',  'J',  'K',  'L',  'M',  'N', 'O',  'P',  'Q',  'R',  'S',  'R',  'U',  'V',  'W',  'X',  'Y',  'Z'];
+//get user input through confirms and prompts
+function getUserInput(){
+  var passwordLength= prompt("how long would you like your password to be?")
+
+
+var hasNumbers= confirm("would you like your password to have numbers? ")
+var upperCase= confirm("would you like capital letters in your password? ")
+
+var lowerCase= confirm ("would you like lowercase letters in your password? ")
+
+var hasSymbols= confirm ("would you like symbols in your password? ")
+
+var userchoice= { 
+  confirmLength: passwordLength,
+  confirmNumbers: hasNumbers,
+  confirmupperCase: upperCase,
+  confirmSymbols: hasSymbols,
+  confirmLowerCase: lowerCase,
+}
+console.log(userchoice)
+return userchoice
+
+}
+
+//create fuction to randomize arrays that we use
+
+//giant function to put it all together
